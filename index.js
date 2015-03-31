f255519f406632f42d15bc8b3f38a862d14620af
Ken Snyder <ken@ken.net>
2015-03-31 12:48:35 +0100

    fixed ember version

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
 	description: 'textarea input that auto-expand/contracts vertically',
 	included: function(app) {
 		this._super.included(app);
-		app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 		app.import('bower_components/expanding-textareas/expanding.js');
 		app.import('vendor/ui-expanding-textarea/ui-expanding-textarea.css');
 	}
